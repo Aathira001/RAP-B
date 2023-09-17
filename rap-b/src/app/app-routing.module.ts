1cba1f33eaca88c3247f60cf8368eff255bf0d3e
Aathira001 <aathirastarz@gmail.com>
2023-09-17 16:56:15 -0400

    adding routing module and paths

--- rap-b/src/app/app-routing.module.ts
+++ rap-b/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatuiComponent } from './components/chatui/chatui.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo: '/chat'},
+  {path: 'chat', component: ChatuiComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
